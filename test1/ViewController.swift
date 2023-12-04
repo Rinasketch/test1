76f23c8ab5476cec66e79d79d5bf6f36d7d9f0f9
Rinasketch <148871412+Rinasketch@users.noreply.github.com>
2023-12-04 16:45:19 +0900

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by Nari Kim on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
